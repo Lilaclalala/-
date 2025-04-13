a7b4bc282adea200e89c3d4efa7246010708d355
takeroneyeah <113002727+takeroneyeah@users.noreply.github.com>
2025-04-13 11:32:44 +0800

    Add files via upload

---
+++ back_end/src/main/java/com/hospital/mapper/CheckMapper.java
@@ -0,0 +1,11 @@
+package com.shanzhu.hospital.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shanzhu.hospital.entity.po.Checks;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface CheckMapper extends BaseMapper<Checks> {
+
+}
