b6b26d94c335ea020bd281a0d9da7ddb8fcefc67
Waymondkkv <skyline0v0@163.com>
2025-04-14 14:20:14 +0800

    Add files via upload

---
+++ back_end/src/main/java/com/hospital/mapper/DrugMapper.java
@@ -0,0 +1,10 @@
+package com.blueachieve.hospital.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.blueachieve.hospital.entity.po.Drug;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DrugMapper extends BaseMapper<Drug> {
+
+}
